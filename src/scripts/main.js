b0e5917a51cb2cf459abe7111ccb3ee6f65e78ab
Lilia Mazur <liliamazur2508@gmail.com>
2022-09-26 14:22:15 +0300

    museum landing page done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const element = document.getElementById('images');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#2') {
+    element.classList.add('gallery__images--action-2');
+  } else {
+    element.classList.remove('gallery__images--action-2');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#3') {
+    element.classList.add('gallery__images--action-3');
+  } else {
+    element.classList.remove('gallery__images--action-3');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#4') {
+    element.classList.add('gallery__images--action-4');
+  } else {
+    element.classList.remove('gallery__images--action-4');
+  }
+});
